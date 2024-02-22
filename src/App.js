6290147b60951ebdfd4c524d6b90432da36e5b4d
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-02-22 23:41:20 +1100

    completed lab

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import About from './components/About.js';
+import NavBar from './components/NavBar.js';
+import Home from './components/Home.js'
 
 function App() {
   return (
